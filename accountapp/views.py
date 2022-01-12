1598735788b7a27319cb887e98563c877fea7509
김유진 <50358936+kluedeugene@users.noreply.github.com>
2022-01-12 16:51:29 +0900

    django course 17 commit about DB saving by POST

--- accountapp/views.py
+++ accountapp/views.py
@@ -2,10 +2,22 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
+from accountapp.models import HelloWorld
+
+
 def hello_world(request):
 
     if request.method == "POST":
-        return render(request, 'accountapp/hello_world.html', context={'text': 'POST METHOD RUN!!'})
+
+        temp = request.POST.get('hello_world_input')
+
+        # 데이터저장 (models.py 에서의 모델사용)
+        new_hello_world = HelloWorld()
+        new_hello_world.text = temp
+        new_hello_world.save()
+
+
+        return render(request, 'accountapp/hello_world.html', context={'hello_world_output': new_hello_world})
     else:
-        return render(request, 'accountapp/hello_world.html', context={'text': 'GET METHOD RUN!!'})
+        return render(request, 'accountapp/hello_world.html', context={'text': 'GET METHOD!!'})
 
