b0a5a4cb61278c1e76291afac3c863a6dd87b566
김유진 <50358936+kluedeugene@users.noreply.github.com>
2022-01-24 17:35:55 +0900

    django course 31 commit about profileapp 구현 시작

--- profileapp/models.py
+++ profileapp/models.py
@@ -6,6 +6,7 @@ from django.db import models
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile')  #CASCADE = 유저탈퇴시 프로필 삭제
 
-    image = models.ImageField(upload_to='proflie/', null=True)
+    image = models.ImageField(upload_to='profile/', null=True)
     nickname = models.CharField(max_length=20, unique=True, null=True)
     message = models.CharField(max_length=100, null=True)
+
