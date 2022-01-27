cc7d48e7d9bfe2a1618267e3c3eb416d014e1f33
김유진 <50358936+kluedeugene@users.noreply.github.com>
2022-01-28 02:03:12 +0900

    django course 25 commit about Articleapp

--- articleapp/models.py
+++ articleapp/models.py
@@ -1,3 +1,13 @@
+from django.contrib.auth.models import User
 from django.db import models
 
 # Create your models here.
+
+class Article(models.Model):
+    writer = models.ForeignKey(User, on_delete=models.SET_NULL, related_name='article', null=True)         #ondelete = 회원탈퇴시 게시글 사라지지않고, 남아잇게
+
+    title = models.CharField(max_length=200, null=True)
+    image = models.ImageField(upload_to='article/', null=False)
+    content = models.TextField(null=True)
+
+    created_at = models.DateField(auto_now_add=True, null=True)
