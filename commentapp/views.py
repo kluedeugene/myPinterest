b165fda240f15252580343c7860b838bc4a1422b
김유진 <50358936+kluedeugene@users.noreply.github.com>
2022-02-07 23:07:02 +0900

    django course 38 commit Commentapp 마무리

--- commentapp/views.py
+++ commentapp/views.py
@@ -1,9 +1,10 @@
-from django.shortcuts import render
 # Create your views here.
 from django.urls import reverse
-from django.views.generic import CreateView
+from django.utils.decorators import method_decorator
+from django.views.generic import CreateView, DeleteView
 
 from articleapp.models import Article
+from commentapp.decorators import comment_ownership_required
 from commentapp.forms import CommentCreationForm
 from commentapp.models import Comment
 
@@ -14,13 +15,23 @@ class CommentCreateView(CreateView):
     template_name = 'commentapp/create.html'
 
     def form_valid(self, form):
-
         temp_comment = form.save(commit=False)
-        temp_comment.article = Article.objects.get(pk=self.request.POST['article_pk'])           #create에서 hideen으로보낸 pk 가 이곳으로 넘어온다.
+        temp_comment.article = Article.objects.get(pk=self.request.POST['article_pk'])   #create에서 hideen으로보낸 pk 가 이곳으로 넘어온다.
         temp_comment.writer = self.request.user
         temp_comment.save()
         return super().form_valid(form)
 
+    def get_success_url(self):
+        return reverse('articleapp:detail', kwargs={'pk': self.object.article.pk})
+
+@method_decorator(comment_ownership_required, 'get')
+@method_decorator(comment_ownership_required, 'post')
+
+class CommentDeleteView(DeleteView):
+    model = Comment
+    context_object_name = 'target_comment'
+    template_name = 'commentapp/delete.html'
 
     def get_success_url(self):
         return reverse('articleapp:detail', kwargs={'pk': self.object.article.pk})
+
