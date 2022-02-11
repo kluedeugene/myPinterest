2575acef85d05c8782078550f0d6e834d7f59936
김유진 <50358936+kluedeugene@users.noreply.github.com>
2022-02-11 21:03:06 +0900

    django 41 course about MultipleObjectMixin을 통한 Projectapp 마무리

--- accountapp/views.py
+++ accountapp/views.py
@@ -8,10 +8,12 @@ from django.shortcuts import render
 from django.urls import reverse, reverse_lazy
 from django.utils.decorators import method_decorator
 from django.views.generic import CreateView, DetailView, UpdateView, DeleteView
+from django.views.generic.list import MultipleObjectMixin
 
 from accountapp.decorators import account_ownership_required
 from accountapp.forms import AccountUpdateForm
 from accountapp.models import HelloWorld
+from articleapp.models import Article
 
 has_ownership = [account_ownership_required, login_required]
 
@@ -41,10 +43,15 @@ class AccountCreateView(CreateView):                        #클래스베이스�
     template_name = 'accountapp/create.html'
 
 
-class AccountDetailView(DetailView):                        #디테일뷰
+class AccountDetailView(DetailView, MultipleObjectMixin):                        #디테일뷰
     model = User
     context_object_name = 'target_user'
     template_name = 'accountapp/detail.html'
+
+    paginate_by = 25
+    def get_context_data(self, **kwargs):
+        object_list = Article.objects.filter(writer=self.get_object())
+        return super(AccountDetailView,self).get_context_data(object_list=object_list, **kwargs)
 
 
 @method_decorator(has_ownership, 'get')                                           #일반 펑션을 사용하는 데코레이터를 메소드에 사용할수잇게 변환해주는 데코레이터?
--- articleapp/models.py
+++ articleapp/models.py
@@ -2,9 +2,12 @@ from django.contrib.auth.models import User
 from django.db import models
 
 # Create your models here.
+from projectapp.models import Project
+
 
 class Article(models.Model):
     writer = models.ForeignKey(User, on_delete=models.SET_NULL, related_name='article', null=True)         #ondelete = 회원탈퇴시 게시글 사라지지않고, 남아잇게
+    project = models.ForeignKey(Project, on_delete=models.SET_NULL, related_name='article', null=True)
 
     title = models.CharField(max_length=200, null=True)
     image = models.ImageField(upload_to='article/', null=False)
