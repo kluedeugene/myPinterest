8e0213fa16ab40a0fc3b62cf5080305913621e55
김유진 <50358936+kluedeugene@users.noreply.github.com>
2022-02-16 03:24:37 +0900

    django course 44 about django course 44 Medium Editor를 이용한  WYSIWYG 작성 기능

--- articleapp/forms.py
+++ articleapp/forms.py
@@ -1,9 +1,15 @@
 from django.forms import ModelForm
+from django import forms
 
 from articleapp.models import Article
+from projectapp.models import Project
 
 
 class ArticleCreationForm(ModelForm):
+    content = forms.CharField(widget=forms.Textarea(attrs={'class': 'editable text-start', #text-left가 start로 바뀌었다(부트스트랩5)
+                                                           'style': 'height: auto;'}))
+    project = forms.ModelChoiceField(queryset=Project.objects.all(), required=False)        #create에서 project필드관련 없어도 작성가능하게
+
     class Meta:
         model = Article
         fields = ['title', 'image', 'project', 'content']
