bf9fc0db5d4d7b9f96211a5894386383ade32113
김유진 <50358936+kluedeugene@users.noreply.github.com>
2022-02-16 16:41:48 +0900

    django course 45 about django course 44 프로젝트 정리 다듬기

--- accountapp/models.py
+++ accountapp/models.py
@@ -2,6 +2,3 @@ from django.db import models
 
 # Create your models here.
 
-
-class HelloWorld(models.Model):
-    text = models.CharField(max_length=255, null=False)  # null = 필요한지 안한지. false= 필요
--- accountapp/views.py
+++ accountapp/views.py
@@ -12,28 +12,9 @@ from django.views.generic.list import MultipleObjectMixin
 
 from accountapp.decorators import account_ownership_required
 from accountapp.forms import AccountUpdateForm
-from accountapp.models import HelloWorld
 from articleapp.models import Article
 
 has_ownership = [account_ownership_required, login_required]
-
-@login_required
-def hello_world(request):
-
-    if request.method == "POST":
-
-        temp = request.POST.get('hello_world_input')
-
-        # 데이터저장 (models.py 에서의 모델사용)
-        new_hello_world = HelloWorld()
-        new_hello_world.text = temp
-        new_hello_world.save()
-
-        hello_world_list = HelloWorld.objects.all()
-        return HttpResponseRedirect(reverse('accountapp:hello_world'))   #projectapp 내부에 있는 hello_world로 재접속 하라는 response
-    else:
-        hello_world_list = HelloWorld.objects.all()
-        return render(request, 'accountapp/hello_world.html', context={'hello_world_list': hello_world_list})
 
 
 class AccountCreateView(CreateView):                        #클래스베이스뷰
@@ -48,7 +29,7 @@ class AccountDetailView(DetailView, MultipleObjectMixin):                       
     context_object_name = 'target_user'
     template_name = 'accountapp/detail.html'
 
-    paginate_by = 25
+    paginate_by = 10
     def get_context_data(self, **kwargs):
         object_list = Article.objects.filter(writer=self.get_object())
         return super(AccountDetailView,self).get_context_data(object_list=object_list, **kwargs)
